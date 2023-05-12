0cdadf291ef61145a15692104e7368966e4fc8ca
Mike <miguel.suarezf05@gmail.com>
2023-05-11 19:09:27 -0500

    Added security to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,32 @@
+import { NextPageContext } from 'next';
+import { signOut, getSession } from 'next-auth/react';
+import useCurrentUser from '@/hooks/useCurrentUser';
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+};
+
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
       <h1 className=" text-neutral-200">Netflix clone</h1>
+      <p className=' text-white'>Logged in as: {user?.name}</p>
+      <button onClick={() => signOut()}>Logout!</button>
     </>
   )
 }
