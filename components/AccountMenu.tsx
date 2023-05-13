82d61ffd67ffebd9c4bc27e3ece4b04941551eec
Mike <miguel.suarezf05@gmail.com>
2023-05-12 23:04:25 -0500

    Created Modal component for billboard and cards

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,3 +1,4 @@
+import useCurrentUser from '@/hooks/useCurrentUser';
 import { signOut } from 'next-auth/react';
 import React from 'react'
 
@@ -6,6 +7,8 @@ interface AccountMenuProps {
 }
 
 const AccountMenu: React.FC<AccountMenuProps> = ({visible}) => {
+  const { data } = useCurrentUser();
+
   if (!visible) {
     return null;
   }
@@ -15,7 +18,7 @@ const AccountMenu: React.FC<AccountMenuProps> = ({visible}) => {
       <div className="flex flex-col gap-3">
         <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
           <img className='w-8 rounded-md' src="/images/default-green.png" alt="profile image" />
-          <p className='text-white text-sm group/item-hover:underline'>Username</p>
+          <p className='text-white text-sm group/item-hover:underline'>{data?.name}</p>
         </div>
         <hr className='bg-gray-600 border-0 h-px my-4' />
         <div onClick={()=>signOut()} className="px-3 text-center text-white text-sm hover:underline">
--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 
 import { BsFillPlayFill } from 'react-icons/bs';
+import { AiOutlineInfo } from 'react-icons/ai';
 import FavoriteButton from "./FavoriteButton";
 import { useRouter } from "next/router";
+import useInfoModal from "@/hooks/useInfoModal";
 
 interface MovieCardProps {
   data: Record<string, any>;
@@ -10,6 +12,8 @@ interface MovieCardProps {
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
   const router = useRouter();
+  const { openModal } = useInfoModal();
+
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw">
       <img
@@ -92,6 +96,26 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
               <BsFillPlayFill size={30}/>
             </div>
             <FavoriteButton movieId={data?.id} />
+            <div 
+              onClick={() => openModal(data?.id)}
+              className="
+                cursor-pointer
+                ml-auto
+                goup/item 
+                w-6 lg:w-10
+                h-6 lg:h-10 
+                border-white 
+                border-2 
+                rounded-full 
+                flex 
+                justify-center 
+                items-center 
+                transition 
+                hover:border-neutral-300
+              "
+            >
+              <AiOutlineInfo className="text-white group-hover/item:text-neutral-300" />
+            </div>
           </div>
 
           <p className="text-green-400 font-semibold mt-4">New <span className="text-white">2023</span></p>
